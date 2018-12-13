36172f77efcc4ecdc421d807d4c98fd585df5e19
katsu1110 <katsuhisa.kawaguchi@uni-tuebingen.de>
2018-12-13 17:17:12 +0100

    getPKA for non-conf split

--- getPKA.m
+++ getPKA.m
@@ -1,10 +1,10 @@
-function [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = getPKA(ss, stm, ch, cf, nbin, pkmethod, repeat)
+function [pka_all, pka_hc, pka_lc] = getPKA(ss, stm, ch, cf, nbin, pkmethod, repeat)
 % compute psychophysical kernel amplitude (PKA) in a specified method
 % INPUT:
 % ss ... signed assigned stimulus (in disparity task, hdx x Dc)
 % stm ... stimulus matrix (trial x frames)
 % ch ... binary choice (0 or 1; trial x 1)
-% cf ... confidence (trial x 1)
+% cf ... confidence (trial x 1). can be empty '[]'.
 % nbin ... the number of time bins for PKA
 % pkamethod ... method to compute PKA 
 % (0; Nienborg & Cumming, 2009; 1: image classification, 2: logistic regression)
@@ -14,9 +14,6 @@ function [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = getPKA(ss
 % pka_all ... averaged PKA
 % pka_hc ... PKA in high confidence trials
 % pka_lc ... PKA in low confidence trials
-% pkavar_all ... variance of pka_all
-% pkavar_hc ... variance of pka_hc
-% pkavar_lc ... variance of pka_lc
 %
 % NOTE: when repeat > 0, resampling is performed and estimated SEM is 
 % stored in the second row of each output pka
@@ -24,10 +21,14 @@ function [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = getPKA(ss
 % +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 % median split of confidence in each stimulus type
-cf = binalize(cf, ss);
+if length(unique(cf)) < 2
+    cf = [];
+else
+    cf = binalize(cf, ss);
+end
 
 % compute PKA
-[pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = ...
+[pka_all, pka_hc, pka_lc] = ...
     computePKA(ss, stm, ch, cf, nbin, pkmethod);
 
 % resampling for error bars
@@ -58,15 +59,14 @@ else
     end
 end
 
-function [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = ...
-    computePKA(ss, stm, ch, cf, nbin, pkmethod)
+function [pka_all, pka_hc, pka_lc] = computePKA(ss, stm, ch, cf, nbin, pkmethod)
 switch pkmethod
     case 0 % Nienborg & Cumming, 2009
-        [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = PKA_hn(ss, stm, ch, cf, nbin);
+        [pka_all, pka_hc, pka_lc] = PKA_hn(ss, stm, ch, cf, nbin);
     case 1 % image classification
-        [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = PKA_ic(ss, stm, ch, cf, nbin);
+        [pka_all, pka_hc, pka_lc] = PKA_ic(ss, stm, ch, cf, nbin);
     case 2 % logistic regression
-        [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = PKA_logreg(stm, ch, cf, nbin);
+        [pka_all, pka_hc, pka_lc] = PKA_logreg(stm, ch, cf, nbin);
 end
 
 function [cf0, cf1] = median_split(cf)
@@ -86,7 +86,7 @@ for a = 1:nbin
     begin = begin + frameperbin;
 end
 
-function [pk, pkvar] = getKernel(stm, disval, ch)
+function pk = getKernel(stm, disval, ch)
 % trial averaged stimulus distributions split by choice
 nd = length(disval);
 ntr = length(ch);
@@ -98,13 +98,14 @@ for r = 1:ntr
 end
 % compute PK for 0% stimulus
 pk = mean(svmat(ch==1,:)) - mean(svmat(ch==0,:));
-pkvar = var(svmat(ch==1, :), [], 1) + var(svmat(ch==0, :), [], 1);
 
-function [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = PKA_hn(ss, stm, ch, cf, nbin)
+function [pka_all, pka_hc, pka_lc] = PKA_hn(ss, stm, ch, cf, nbin)
 % only 0% signal trials
 stm = stm(ss==0,:);
 ch = ch(ss==0);
-cf = cf(ss==0);
+if ~isempty(cf)
+    cf = cf(ss==0);
+end
 % time-averaged PK in each time bin
 disval = unique(stm);
 nd = length(disval);
@@ -118,72 +119,64 @@ nd = length(disval);
 tkernel = nan(nd, nbin);
 tkernel_h = nan(nd, nbin);
 tkernel_l = nan(nd, nbin);
-tkernelvar = nan(nd, nbin);
-tkernelvar_h = nan(nd, nbin);
-tkernelvar_l = nan(nd, nbin);
 begin = 1;
 nframe = size(stm, 2);
 frameperbin = floor(nframe/nbin);
 for a = 1:nbin
-    [pk0, pk0var] = getKernel(stm(:,begin:begin+frameperbin-1), disval, ch);
-    [pkh, pkhvar] = getKernel(stm(cf == 1, begin:begin+frameperbin-1), disval, ch(cf == 1));
-    [pkl, pklvar] = getKernel(stm(cf == 0, begin:begin+frameperbin-1), disval, ch(cf == 0));
+    pk0 = getKernel(stm(:,begin:begin+frameperbin-1), disval, ch);
     tkernel(:,a) = pk0';
-    tkernel_h(:,a) = pkh';
-    tkernel_l(:,a) = pkl';
-    tkernelvar(:,a) = pk0var';
-    tkernelvar_h(:,a) = pkhvar';
-    tkernelvar_l(:,a) = pklvar';
+    if ~isempty(cf)
+        pkh = getKernel(stm(cf == 1, begin:begin+frameperbin-1), disval, ch(cf == 1));
+        pkl = getKernel(stm(cf == 0, begin:begin+frameperbin-1), disval, ch(cf == 0));
+        tkernel_h(:,a) = pkh';
+        tkernel_l(:,a) = pkl';
+    end
     begin = begin + frameperbin;
 end
 % PKA
 % pk = getKernel(stm, disval, ch);
 pk = mean(tkernel,2);
-pkvar = mean(tkernelvar(:));
 pka_all = nan(1,nbin);
 pka_hc = nan(1,nbin);
 pka_lc = nan(1,nbin);
-pkavar_all = nan(1,nbin);
-pkavar_hc = nan(1,nbin);
-pkavar_lc = nan(1,nbin);
 for a = 1:nbin
     pka_all(a) = dot(tkernel(:,a), pk);
-    pka_hc(a) = dot(tkernel_h(:,a), pk);
-    pka_lc(a) = dot(tkernel_l(:,a), pk);
-    pkavar_all(a) = mean(tkernelvar(:,a),1)*pkvar;
-    pkavar_hc(a) = mean(tkernelvar_h(:,a),1)*pkvar;
-    pkavar_lc(a) = mean(tkernelvar_l(:,a),1)*pkvar;
+    if ~isempty(cf)
+        pka_hc(a) = dot(tkernel_h(:,a), pk);
+        pka_lc(a) = dot(tkernel_l(:,a), pk);
+    end
 end
 
-function [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = PKA_ic(ss, stm, ch, cf, nbin)
+function [pka_all, pka_hc, pka_lc] = PKA_ic(ss, stm, ch, cf, nbin)
 % only 0% signal trials
 stm = stm(ss==0,:);
 ch = ch(ss==0);
-cf = cf(ss==0);
+if ~isempty(cf)
+    cf = cf(ss==0);
+end
 % image classification to compute PKA
 pka_all = mean(stm(ch==1,:), 1) - mean(stm(ch==0,:), 1);
-pka_hc = mean(stm(ch==1 & cf==1,:), 1) - mean(stm(ch==0 & cf==1,:), 1);
-pka_lc = mean(stm(ch==1 & cf==0,:), 1) - mean(stm(ch==0 & cf==0,:), 1);
 pka_all = binstm(pka_all, nbin);
-pka_hc = binstm(pka_hc, nbin);
-pka_lc = binstm(pka_lc, nbin);
-pkavar_all = var(stm(ch==1,:), [], 1) + var(stm(ch==0,:), [], 1);
-pkavar_hc = var(stm(ch==1 & cf==1,:), [], 1) + var(stm(ch==0 & cf==1,:), [], 1);
-pkavar_lc = var(stm(ch==1 & cf==0,:), [], 1) + var(stm(ch==0 & cf==0,:), [], 1);
+if ~isempty(cf)
+    pka_hc = mean(stm(ch==1 & cf==1,:), 1) - mean(stm(ch==0 & cf==1,:), 1);
+    pka_lc = mean(stm(ch==1 & cf==0,:), 1) - mean(stm(ch==0 & cf==0,:), 1);
+    pka_hc = binstm(pka_hc, nbin);
+    pka_lc = binstm(pka_lc, nbin);
+end
 
-function [pka_all, pka_hc, pka_lc, pkavar_all, pkavar_hc, pkavar_lc] = PKA_logreg(stm, ch, cf, nbin)
+function [pka_all, pka_hc, pka_lc] = PKA_logreg(stm, ch, cf, nbin)
 % logistic regression (intercept included to capture a bias)
 stm = zscore(binstm(stm, nbin));
 pka_all = glmfit(stm, ch, ...
     'binomial', 'link', 'logit', 'constant', 'on');
-pka_hc = glmfit(stm(cf == 1, :), ch(cf == 1), ...
-    'binomial', 'link', 'logit', 'constant', 'on');
-pka_lc = glmfit(stm(cf == 0, :), ch(cf == 0), ...
-    'binomial', 'link', 'logit', 'constant', 'on');
-pka_all = pka_all(2:end)'; pka_hc = pka_hc(2:end)'; pka_lc = pka_lc(2:end)';
-pkavar_all = var(stm(ch==1,:), [], 1) + var(stm(ch==0,:), [], 1);
-pkavar_hc = var(stm(ch==1 & cf==1,:), [], 1) + var(stm(ch==0 & cf==1,:), [], 1);
-pkavar_lc = var(stm(ch==1 & cf==0,:), [], 1) + var(stm(ch==0 & cf==0,:), [], 1);
+pka_all = pka_all(2:end)'; 
+if ~isempty(cf)
+    pka_hc = glmfit(stm(cf == 1, :), ch(cf == 1), ...
+        'binomial', 'link', 'logit', 'constant', 'on');
+    pka_lc = glmfit(stm(cf == 0, :), ch(cf == 0), ...
+        'binomial', 'link', 'logit', 'constant', 'on');
+    pka_hc = pka_hc(2:end)'; pka_lc = pka_lc(2:end)';
+end
 
 function [err, err0, err1] = resamplePK(ss, stm, ch, cf, nbin, repeat, pkmethod)
 % resampling procedure
@@ -192,9 +185,13 @@ ampr0 = nan(repeat, nbin);
 ampr1 = nan(repeat, nbin);
 ntr = length(ch);
 for r = 1:repeat
-    disp(r)
-    rtr = randi(ntr, ntr, 1);
-    [ampr(r,:), ampr1(r,:), ampr0(r,:)] = computePKA(ss(rtr), stm(rtr,:), ch(rtr), cf(rtr), nbin, pkmethod);
+%     disp(r)
+    rtr = randi(ntr, ntr, 1);    
+    if ~isempty(cf)
+        [ampr(r,:), ampr1(r,:), ampr0(r,:)] = computePKA(ss(rtr), stm(rtr,:), ch(rtr), cf(rtr), nbin, pkmethod);
+    else
+        [ampr(r,:), ampr1(r,:), ampr0(r,:)] = computePKA(ss(rtr), stm(rtr,:), ch(rtr), [], nbin, pkmethod);
+    end
 end
 err = std(ampr, [], 1);
 err0 = std(ampr0, [], 1);
